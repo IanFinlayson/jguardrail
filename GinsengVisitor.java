524ebdf3b7d0cef878645fe97968f2aff0649bc1
Ian Finlayson <ifinlay@umw.edu>
2023-10-03 11:17:55 -0400

    OMG this will be easyy!!!11

--- GinsengVisitor.java
+++ GinsengVisitor.java
@@ -1,10 +1,34 @@
+import java.util.List;
 
 public class GinsengVisitor extends JavaParserBaseVisitor<Double> {
     
 
+    // returns true if a break was found
+    public boolean checkForBreak(JavaParser.StatementContext stmt) {
+        // if there is no statement, it could be a declaration or sth
+        if (stmt == null) {
+            return false;
+        }
+        if (stmt.BREAK() != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	@Override public Double visitSwitchBlockStatementGroup(JavaParser.SwitchBlockStatementGroupContext ctx) {
+	@Override
+    public Double visitSwitchBlockStatementGroup(JavaParser.SwitchBlockStatementGroupContext ctx) {
         System.out.println("Saw a switch block statement group!");
+		
+		for (JavaParser.BlockStatementContext stmt: ctx.blockStatement()) {
+            // TODO what if it's in a nested {}??
+            if (checkForBreak(stmt.statement())) {
+                System.out.println("\tYES! a break!");
+            } else {
+                System.out.println("\t...");
+            }
+        }
+        
         return visitChildren(ctx);
     }
 
--- tests/Test1.java
+++ tests/Test1.java
@@ -6,11 +6,15 @@ public class Test1 {
 
         switch (in.nextInt()) {
             case 0:
+                int y;
                 System.out.println("zero");
+                break;
             case 1:
                 System.out.println("one");
+                {System.out.println("a"); System.out.println("b");System.out.println("c");}
             case 2:
                 System.out.println("two");
+                break;
             default:
                 System.out.println("rather more");
         }
