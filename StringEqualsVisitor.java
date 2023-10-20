2bf68422cf7c11e68596d6d145182979b956bbbe
Ian Finlayson <ifinlay@umw.edu>
2023-10-20 17:19:29 -0400

    Got the string equals working -- only for literals.  Probably too hard in general

--- StringEqualsVisitor.java
+++ StringEqualsVisitor.java
@@ -2,14 +2,20 @@ import java.util.List;
 
 public class StringEqualsVisitor extends JavaParserBaseVisitor<Void> {
 
+
     // hook into the antlr visitor system to catch using == with strings
     @Override
     public Void visitExpression(JavaParser.ExpressionContext expr) {
        
-        if (expr.EQUAL() != null) {
-            System.out.println("Found an == operator!");
-        } else if (expr.NOTEQUAL() != null) {
-            System.out.println("Found a != operator!");
+        if ((expr.EQUAL() != null) || (expr.NOTEQUAL() != null)) {
+            JavaParser.ExpressionContext lhs = expr.expression(0), rhs = expr.expression(1);
+
+            // if either side is a string literal, flag it
+            if ((lhs.primary() != null && lhs.primary().literal() != null && lhs.primary().literal().STRING_LITERAL() != null) ||
+                (rhs.primary() != null && rhs.primary().literal() != null && rhs.primary().literal().STRING_LITERAL() != null)) {
+
+                System.out.println("Warning: using the == operator between string literals on line " + expr.getStart().getLine());
+            }
         }
         
         // recurse (in order to get switches that may be nested themselves)
