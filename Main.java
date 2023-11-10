dd2d45c8ff6649340f017444d66332e4cd6210a7
Ian Finlayson <ifinlay@umw.edu>
2023-11-10 13:09:18 -0500

    Make it so the program can be run on multiple .java files at once

--- Main.java
+++ Main.java
@@ -7,40 +7,37 @@ import org.antlr.v4.runtime.tree.ParseTreeWalker;
 public class Main {
 
     public static void main(String[] args) {
-        // get input file
-        if (args.length != 1) {
-            System.out.println("Please pass the Java file as input.");
-            return;
-        }
+        // for each input file given to us
+        for (int i = 0; i < args.length; i++) {
+            // set up streams
+            JavaLexer lexer = null;
+            try {
+                lexer = new JavaLexer(CharStreams.fromFileName(args[i]));
+                Warnings.setFileName(args[i]);
+            } catch (Exception e) {
+                System.out.println("Could not open '" + args[i] + "' for reading.");
+                return;
+            }
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            JavaParser parser = new JavaParser(tokens);
+            
+            // do the parsing
+            ParseTree tree = parser.compilationUnit();
 
-        // set up streams
-        JavaLexer lexer = null;
-        try {
-            lexer = new JavaLexer(CharStreams.fromFileName(args[0]));
-            Warnings.setFileName(args[0]);
-        } catch (Exception e) {
-            System.out.println("Could not open '" + args[0] + "' for reading.");
-            return;
-        }
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        JavaParser parser = new JavaParser(tokens);
-        
-        // do the parsing
-        ParseTree tree = parser.compilationUnit();
+            // we make a list of all the checks we have
+            JavaParserBaseVisitor checkers [] = {
+                new SwitchCheckVisitor(),
+                new StringEqualsVisitor(),
+                new IntDivideVisitor(),
+                new VoidConstructorVisitor(),
+                new ShadowCheckVisitor(),
+                new SelfSetVisitor()
+            };
 
-        // we make a list of all the checks we have
-        JavaParserBaseVisitor checkers [] = {
-            new SwitchCheckVisitor(),
-            new StringEqualsVisitor(),
-            new IntDivideVisitor(),
-            new VoidConstructorVisitor(),
-            new ShadowCheckVisitor(),
-            new SelfSetVisitor()
-        };
-
-        // run all the checkers
-        for (JavaParserBaseVisitor checker : checkers) {
-            checker.visit(tree);
+            // run all the checkers
+            for (JavaParserBaseVisitor checker : checkers) {
+                checker.visit(tree);
+            }
         }
     }
 }
